193d884e8a16ba4ac7ad03a1d9327cfdc8c0b729
anan0531 <jinyan0531@outlook.com>
2017-11-26 20:21:09 +0800

    增加binlogJson

--- cmd/go-mysqlbinlog/main.go
+++ cmd/go-mysqlbinlog/main.go
@@ -1,28 +1,27 @@
-// go-mysqlbinlog: a simple binlog tool to sync remote MySQL binlog.
-// go-mysqlbinlog supports semi-sync mode like facebook mysqlbinlog.
-// see http://yoshinorimatsunobu.blogspot.com/2014/04/semi-synchronous-replication-at-facebook.html
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"os"
+	_"os"
 
 	"github.com/juju/errors"
 	"github.com/siddontang/go-mysql/mysql"
 	"github.com/siddontang/go-mysql/replication"
+
+	_"os"
 )
 
-var host = flag.String("host", "127.0.0.1", "MySQL host")
-var port = flag.Int("port", 3306, "MySQL port")
+var host = flag.String("host", "192.168.18.17", "MySQL host")
+var port = flag.Int("port", 30010, "MySQL port")
 var user = flag.String("user", "root", "MySQL user, must have replication privilege")
-var password = flag.String("password", "", "MySQL password")
+var password = flag.String("password", "123456", "MySQL password")
 
 var flavor = flag.String("flavor", "mysql", "Flavor: mysql or mariadb")
 
 var file = flag.String("file", "", "Binlog filename")
-var pos = flag.Int("pos", 4, "Binlog position")
+var pos = flag.Int("pos", 1516930, "Binlog position")
 
 var semiSync = flag.Bool("semisync", false, "Support semi sync")
 var backupPath = flag.String("backup_path", "", "backup path to store binlog files")
@@ -61,14 +60,37 @@ func main() {
 			return
 		}
 
+
 		for {
 			e, err := s.GetEvent(context.Background())
 			if err != nil {
 				fmt.Printf("Get event error: %v\n", errors.ErrorStack(err))
 				return
 			}
+			switch (e.Header.EventType){
+			case replication.WRITE_ROWS_EVENTv2:
+				fmt.Println(e.Header.EventType)
+				fmt.Println(e.Header.LogPos)
 
-			e.Dump(os.Stdout)
+				fmt.Println(e.Json())
+			case replication.DELETE_ROWS_EVENTv2:
+				fmt.Println(e.Header.EventType)
+				fmt.Println(e.Header.LogPos)
+
+				fmt.Println(e.Json())
+			case replication.UPDATE_ROWS_EVENTv2:
+				fmt.Println(e.Header.EventType)
+				fmt.Println(e.Header.LogPos)
+
+				fmt.Println(e.Json())
+
+
+
+			}
+			//e.Header.Dump(os.Stdout)
+			//fmt.Println(e.Json())
+			//e.Event.Dump(os.Stdout)
+			//e.Dump(os.Stdout)
 		}
 	}
 
